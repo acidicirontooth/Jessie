555252d229b55059a6329ce5e2e63e7d11814abc
acidicirontooth <acidicirontooth@gmail.com>
2016-11-08 17:03:43 -0700

    Add files via upload

---
+++ jessie.js
@@ -0,0 +1,26 @@
+// jessie.js - build 161108
+function Jessie(library) {
+  this.library = library;
+
+  this.expand = function(start, expansion) {
+    if(Jessie.library[start]) {
+      var pick = random(Jessie.library[start]);
+      for (var i = 0; i < pick.length; i++) {
+        Jessie.expand(pick[i], expansion);
+      }
+    } else {
+      expansion.push(start);
+    }
+    return expansion.join(" ");
+  }
+
+  this.cfg = function () {
+    var start = "<start>";
+    var expansion = []
+    var result = Jessie.expand(start, expansion);
+  }
+}
+
+var Jessie = {
+  BUILD: 161108
+}
